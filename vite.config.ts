55a7a9fa908634883af7a402bef2ee73183b400d
olabamipetaiwo <teeola48@gmail.com>
2025-04-16 18:20:36 +0100

    dev::change run sequence

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,8 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
+    mode === 'development' 
+    &&
     componentTagger(),
   ].filter(Boolean),
   resolve: {
